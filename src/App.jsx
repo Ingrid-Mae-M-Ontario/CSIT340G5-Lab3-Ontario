78533dcae6627604bb576d6e83af1634dfe39035
ingbirb <ingridmaeontario@gmail.com>
2026-09-21 17:37:11 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.units}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.units1}
-      </p>
-      <p>
-        {props.part2} {props.units2}
-      </p>
-      <p>
-        {props.part3} {props.units3}
-      </p>
+      <Part part={props.part1} units={props.units1} />
+      <Part part={props.part2} units={props.units2} />
+      <Part part={props.part3} units={props.units3} />
     </div>
   )
 }
